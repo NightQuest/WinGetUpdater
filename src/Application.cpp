d90d54a3a1b9fb4902311a271bf791f98bcd8056
Amelia <NightQuest@users.noreply.github.com>
2021-06-17 19:55:13 -0700

    Base code

---
+++ src/Application.cpp
@@ -0,0 +1,6 @@
+#include "preCompiled.h"
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	return 0;
+}
---
+++ src/preCompiled.h
@@ -0,0 +1,6 @@
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include "Application.h"
+#include "WinGetHandler.h"
